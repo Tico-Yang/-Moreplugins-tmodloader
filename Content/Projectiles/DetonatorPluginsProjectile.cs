cbe64c4d63c6eedac11c27e9b0680440d79294ad
nanianjinxiu <liaojin20010212@163.com>
2026-03-02 22:58:06 +0800

    补弹幕

---
+++ Content/Projectiles/DetonatorPluginsProjectile.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Moreplugins.Content.Projectiles
+{
+    internal class DetonatorPluginsProjectile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.PlayerHurtDamageIgnoresDifficultyScaling[Type] = true;
+            ProjectileID.Sets.Explosive[Type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.timeLeft = 5;
+            Projectile.width = 14;
+            Projectile.height = 14;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.penetrate = -1;
+        }
+
+        public override void AI()
+        {
+            if (Projectile.owner == Main.myPlayer && Projectile.timeLeft <= 3)
+            {
+                Projectile.PrepareBombToBlow();
+            }
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            if (Projectile.velocity.X != oldVelocity.X)
+            {
+                Projectile.velocity.X = oldVelocity.X * -0.4f;
+            }
+
+            if (Projectile.velocity.Y != oldVelocity.Y && oldVelocity.Y > 0.7f)
+            {
+                Projectile.velocity.Y = oldVelocity.Y * -0.4f;
+            }
+
+            return false;
+        }
+
+        public override void PrepareBombToBlow()
+        {
+            Projectile.tileCollide = false;
+            Projectile.alpha = 255;
+            Projectile.Resize(512, 512);
+            Projectile.knockBack = 8f;
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            SoundEngine.PlaySound(SoundID.Item62, Projectile.position);
+
+            for (int i = 0; i < 120; i++)
+            {
+                var smoke = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Smoke, 0f, 0f, 100, default, 1.5f);
+                smoke.velocity *= 1.4f;
+            }
+
+            for (int j = 0; j < 60; j++)
+            {
+                var fireDust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Torch, 0f, 0f, 100, default, 3.5f);
+                fireDust.noGravity = true;
+                fireDust.velocity *= 7f;
+                fireDust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Torch, 0f, 0f, 100, default, 1.5f);
+                fireDust.velocity *= 3f;
+            }
+
+            for (int k = 0; k < 6; k++)
+            {
+                float speedMulti = 0.4f;
+                if (k == 1)
+                {
+                    speedMulti = 0.8f;
+                }
+
+                var smokeGore = Gore.NewGoreDirect(Projectile.GetSource_Death(), Projectile.position, default, Main.rand.Next(GoreID.Smoke1, GoreID.Smoke3 + 1));
+                smokeGore.velocity *= speedMulti;
+                smokeGore.velocity += Vector2.One;
+                smokeGore = Gore.NewGoreDirect(Projectile.GetSource_Death(), Projectile.position, default, Main.rand.Next(GoreID.Smoke1, GoreID.Smoke3 + 1));
+                smokeGore.velocity *= speedMulti;
+                smokeGore.velocity.X -= 1f;
+                smokeGore.velocity.Y += 1f;
+                smokeGore = Gore.NewGoreDirect(Projectile.GetSource_Death(), Projectile.position, default, Main.rand.Next(GoreID.Smoke1, GoreID.Smoke3 + 1));
+                smokeGore.velocity *= speedMulti;
+                smokeGore.velocity.X += 1f;
+                smokeGore.velocity.Y -= 1f;
+                smokeGore = Gore.NewGoreDirect(Projectile.GetSource_Death(), Projectile.position, default, Main.rand.Next(GoreID.Smoke1, GoreID.Smoke3 + 1));
+                smokeGore.velocity *= speedMulti;
+                smokeGore.velocity -= Vector2.One;
+            }
+        }
+    }
+}
